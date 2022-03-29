449ce45b24a24e45ee310c8986ff6ae75ae7dbc7
Vezeveer <mortalmanny@live.com>
2022-03-29 18:41:09 +0800

    front looks partly done

--- app/src/main/java/com/bignerdranch/android/dicemakeitroll/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/dicemakeitroll/MainActivity.kt
@@ -2,10 +2,23 @@ package com.bignerdranch.android.dicemakeitroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun roll(view: android.view.View) {
+        Toast.makeText(this, "yooo", Toast.LENGTH_LONG).show()
+    }
+
+    fun addDice(view: View) {
+
+    }
+
+
 }
