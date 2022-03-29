b88717745543eb80f783e7be5994a901241c1a9b
Vezeveer <mortalmanny@live.com>
2022-03-29 19:59:02 +0800

    added 2 dice, and roll code

--- app/src/main/java/com/bignerdranch/android/dicemakeitroll/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/dicemakeitroll/MainActivity.kt
@@ -2,8 +2,8 @@ package com.bignerdranch.android.dicemakeitroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +13,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun roll(view: android.view.View) {
-        Toast.makeText(this, "yooo", Toast.LENGTH_LONG).show()
+        val imgDice1 = findViewById<ImageView>(R.id.img_first_dice)
+        val imgDice2 = findViewById<ImageView>(R.id.img_second_dice)
+        val tvTotal = findViewById<TextView>(R.id.tvTotal)
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+
+        var diceA = dice1.roll()
+        var diceB = dice2.roll()
+
+        tvTotal.setText(""+(diceA+diceB))
+
+        when(diceA){
+            1 -> imgDice1.setImageResource(R.drawable.d1)
+            2 -> imgDice1.setImageResource(R.drawable.d2)
+            3 -> imgDice1.setImageResource(R.drawable.d3)
+            4 -> imgDice1.setImageResource(R.drawable.d4)
+            5 -> imgDice1.setImageResource(R.drawable.d5)
+            6 -> imgDice1.setImageResource(R.drawable.d6)
+        }
+
+        when(diceB){
+            1 -> imgDice2.setImageResource(R.drawable.d1)
+            2 -> imgDice2.setImageResource(R.drawable.d2)
+            3 -> imgDice2.setImageResource(R.drawable.d3)
+            4 -> imgDice2.setImageResource(R.drawable.d4)
+            5 -> imgDice2.setImageResource(R.drawable.d5)
+            6 -> imgDice2.setImageResource(R.drawable.d6)
+        }
+
+
     }
-
-    fun addDice(view: View) {
-
-    }
-
-
 }
